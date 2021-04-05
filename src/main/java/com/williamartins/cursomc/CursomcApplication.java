2578e7299eb1359f8cc367a1f60686cb0fc6828e
William Martins <williamartins02@hotmail.com>
2021-04-05 14:06:12 -0300

    Projeto criado

---
+++ src/main/java/com/williamartins/cursomc/CursomcApplication.java
@@ -0,0 +1,15 @@
+package com.williamartins.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/*Classe prncipal para executar o projeto.*/
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
