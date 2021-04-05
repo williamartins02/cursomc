0b0832dc7f374e7c78d853f999ce4c1c71b39b79
William Martins <williamartins02@hotmail.com>
2021-04-05 15:44:32 -0300

    Testando a primeira classe de dominio

---
+++ src/main/java/com/williamartins/cursomc/domain/Categoria.java
@@ -0,0 +1,77 @@
+package com.williamartins.cursomc.domain;
+
+import java.io.Serializable;
+
+/*implementar o SERIALIZABLE 
+ * é uma interface que diz que a class sendo implementada "categoria"
+ *os objeto dela pode ser convertido com uma sequancia de byte,
+ *pra que serve ? - Serve pra dizer q os objeto pode ser gravado em arquivos, trafegar em redes etc..
+ *exigencia da linguagem java.
+ *.".*/
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	/* atributos da class*/
+	private Integer id;
+	private String nome;
+	
+	/*Criando um construtor vazio, ou seja com o construtor 
+	 * vazio estancio um objeto sem jogar nada os atributos */
+	public Categoria() {
+	}
+	/*criando um construtor para povoar os dados do atributos*/
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	/*Getters e Setters metodos de acesso ao atributos.*/
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	/*comparar os objeto com seus conteudo "Hascode".*/
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+}
--- src/main/java/com/williamartins/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/williamartins/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,31 @@
 package com.williamartins.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import com.williamartins.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String Listar() {
-		return "Rest esta funcionando!";
+	public List<Categoria> Listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informámatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+		
 	}
 
 	
